3334c3797b8e0b6032873836caa358a28620e8bf
zgospodinov <zdravko.gospodinov@gmail.com>
2022-08-15 11:48:47 +0300

    Fix postgres version issue

--- API/Startup.cs
+++ API/Startup.cs
@@ -92,6 +92,7 @@ namespace API
 
                 // Whether the connection string came from the local development configuration file
                 // or from the environment variable from Heroku, use it to set up your DbContext.
+                AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
                 options.UseNpgsql(connStr);
             });
 
